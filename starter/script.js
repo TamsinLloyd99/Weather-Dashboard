683c35e2644dd72ad33d3a713a4a177103ed1659
Tamsin Lloyd <tamsin_lloyd@hotmail.com>
2024-01-27 22:44:24 +0000

    added fetch code to get current and future weather forecast

--- starter/script.js
+++ starter/script.js
@@ -2,34 +2,49 @@ var currentWeatherEL = $('today');
 var futureWeatherEL = $('forecast');
 
 var API_KEY = 'b6607d95fdf680a89ffd501aedb4f29a';
-var city = $('form-input').val();
+var city = "London";//needs changing to match search box
 var todaysForecastURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`;
-var futureForecastUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
-var iconURL = `https://openweathermap.org/img/wn/${icon}@2x.png`;
-console.log(queryUrl)
 
+// var iconURL = `https://openweathermap.org/img/wn/${icon}@2x.png`;
+
+let lat;
+let lon;
 
 
 
 fetch(todaysForecastURL)
-    .then(function (response) {
-      return response.json();
+.then(function (response) {
+ return response.json();
     })
     .then(function (data) {
-        console.log(data);
-    })
+        console.log("Current Weather",data);
+lat = data.coord.lat;
+lon = data.coord.lon;
+// console.log(lat);
+// console.log(lon);
+weatherPrediction(lat,lon);
 
-var longitute;
+var futureForecastUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
 
-var latitude;
-
-  fetch(futureForecastUrl)
-    .then(function (response) {
-      return response.json();
-    })
+fetch(futureForecastUrl)
+.then(function (response) {
+return response.json();
+})
     .then(function (data) {
-      console.log(data);
+        console.log("Future Forecast",data);
+    
     })
+    })
+
+
+
+
+
+function weatherPrediction (lat,lon){
+  console.log("Latitude and Longitude" , lat,lon);
+}
+
+//   
 
 
 
